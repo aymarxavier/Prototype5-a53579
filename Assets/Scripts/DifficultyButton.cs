08fa4b1926b12b463cf3ef2ba530bc9b44d29d3d
aymarxavier <a53579@alunos.ipb.pt>
2024-06-14 22:55:53 +0100

    tutotial chalange 5

---
+++ Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+    private GameManage gameManager;
+    public int difficulty;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManage>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + "was clicked");
+        gameManager.StartGame(difficulty);
+    }
+    
+
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManage : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public bool isGameActive;
     private float spawnRate = 1.0f;
     private int score;
 
@@ -14,9 +20,8 @@ public class GameManage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+        
+        
     }
 
     // Update is called once per frame
@@ -26,7 +31,7 @@ public class GameManage : MonoBehaviour
     }
     IEnumerator SpawnTarget()
     {
-        while(true)
+        while(isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -39,5 +44,26 @@ public class GameManage : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive=false;
+        restartButton.gameObject.SetActive(true);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
+    }
+    public void StarGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
     }
 }
